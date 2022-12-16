5c2d32be2ee9a77186cd8cc545a9ba7c4873a15f
Tod Oliver <olivertod11@yahoo.de>
2022-12-16 10:34:54 +0100

    Add dynamic table component

---
+++ projects/ngx-gepardec-mat/src/lib/components/dynamic-table/dynamic-table.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'gpx-dynamic-table',
+  templateUrl: './dynamic-table.component.html',
+  styleUrls: ['./dynamic-table.component.css']
+})
+export class DynamicTableComponent {
+
+}
---
+++ projects/ngx-gepardec-mat/src/lib/components/dynamic-table/dynamic-table.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DynamicTableComponent} from './dynamic-table.component';
+
+
+@NgModule({
+  declarations: [
+    DynamicTableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DynamicTableModule {
+}
