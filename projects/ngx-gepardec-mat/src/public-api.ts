546fa5e864f5e1acbb7f19843ea4c54e08ff57e6
Tod Oliver <olivertod11@yahoo.de>
2022-12-16 12:11:09 +0100

    Remove generated components and structure public-api

--- projects/ngx-gepardec-mat/src/public-api.ts
+++ projects/ngx-gepardec-mat/src/public-api.ts
@@ -2,13 +2,6 @@
  * Public API Surface of ngx-gepardec-mat
  */
 
-export * from './lib/ngx-gepardec-mat.service';
-export * from './lib/ngx-gepardec-mat.component';
-export * from './lib/ngx-gepardec-mat.module';
-
-
-export * from './lib/components/dynamic-table/dynamic-table.module'
-export * from './lib/components/dynamic-table/dynamic-table.component'
-
+// Dynamic table
 export * from './lib/components/dynamic-table/dynamic-table.component';
 export * from './lib/components/dynamic-table/dynamic-table.module';
