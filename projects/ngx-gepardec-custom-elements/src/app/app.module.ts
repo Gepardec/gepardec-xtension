3fbeef9fd61144242bd0e79c28b5d9c0e5be9013
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2023-01-27 11:44:06 +0100

    add custom elements

--- projects/ngx-gepardec-custom-elements/src/app/app.module.ts
+++ projects/ngx-gepardec-custom-elements/src/app/app.module.ts
@@ -7,10 +7,30 @@ import {createCustomElement} from '@angular/elements';
 import {
   InlineTextEditorComponent
 } from '../../../ngx-gepardec-mat/src/lib/components/inline-text-editor/inline-text-editor.component';
-import {AngularMaterialModule} from '../../../dev-playground/src/app/angular-material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from '../../../dev-playground/src/app/app-routing.module';
 import {NavigationModule} from '../../../dev-playground/src/app/navigation/navigation.module';
+import {
+  InlineTextEditorModule
+} from '../../../ngx-gepardec-mat/src/lib/components/inline-text-editor/inline-text-editor.module';
+import {DynamicTableModule} from '../../../ngx-gepardec-mat/src/lib/components/dynamic-table/dynamic-table.module';
+import {FileUploadModule} from '../../../ngx-gepardec-mat/src/lib/components/file-upload/file-upload.module';
+import {
+  FileUploadDynamicTableModule
+} from '../../../dev-playground/src/app/components/file-upload-dynamic-table/file-upload-dynamic-table.module';
+import {
+  ExpandableDynamicTableModule
+} from '../../../ngx-gepardec-mat/src/lib/components/expandable-dynamic-table/expandable-dynamic-table.module';
+import {
+  DynamicTableComponent
+} from '../../../ngx-gepardec-mat/src/lib/components/dynamic-table/dynamic-table.component';
+import {FileUploadComponent} from '../../../ngx-gepardec-mat/src/lib/components/file-upload/file-upload.component';
+import {
+  FileUploadDynamicTableComponent
+} from '../../../dev-playground/src/app/components/file-upload-dynamic-table/file-upload-dynamic-table.component';
+import {
+  ExpandableDynamicTableComponent
+} from '../../../ngx-gepardec-mat/src/lib/components/expandable-dynamic-table/expandable-dynamic-table.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +41,11 @@ import {NavigationModule} from '../../../dev-playground/src/app/navigation/navig
     BrowserAnimationsModule,
     AppRoutingModule,
     NavigationModule,
-    AngularMaterialModule
+    DynamicTableModule,
+    InlineTextEditorModule,
+    FileUploadModule,
+    FileUploadDynamicTableModule,
+    ExpandableDynamicTableModule
   ],
   providers: []
 })
@@ -31,7 +55,11 @@ export class AppModule implements DoBootstrap {
   }
 
   ngDoBootstrap() {
+    this.createAndDefineCustomElement('gpx-dynamic-table', DynamicTableComponent);
     this.createAndDefineCustomElement('gpx-inline-text-editor', InlineTextEditorComponent);
+    this.createAndDefineCustomElement('gpx-file-upload', FileUploadComponent);
+    this.createAndDefineCustomElement('gpx-file-upload-dynamic-table', FileUploadDynamicTableComponent);
+    this.createAndDefineCustomElement('gpx-expandable-dynamic-table', ExpandableDynamicTableComponent);
   }
 
   createAndDefineCustomElement(selector: string, component: Type<CustomElementsBase>) {
