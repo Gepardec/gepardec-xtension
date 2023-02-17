f05230070ec620ce5c2c19020e9dc06aab571944
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2023-02-17 11:44:51 +0100

    add cypress test

--- projects/ngx-gepardec-mat/src/lib/components/inline-text-editor/inline-text-editor.component.cy.ts
+++ projects/ngx-gepardec-mat/src/lib/components/inline-text-editor/inline-text-editor.component.cy.ts
@@ -1,6 +1,21 @@
-import {InlineTextEditorComponent} from "./inline-text-editor.component";
+import {InlineTextEditorComponent} from './inline-text-editor.component';
+
 describe('InlineTextEditorComponent', () => {
   it('mounts', () => {
     cy.mount(InlineTextEditorComponent);
-  })
+  });
+
+  it('#onStartEditing - should show form-field', () => {
+    cy.mount(InlineTextEditorComponent, {
+      componentProperties: {
+        value: 'gepardec-extension',
+      },
+    })
+
+    cy.findByTestId('add-button').should('not.exist');
+    cy.findByTestId('form-field').should('not.exist');
+
+    cy.findByTestId('text-field').click();
+    cy.findByTestId('form-field').should('exist');
+  });
 })
